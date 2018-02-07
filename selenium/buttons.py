c56c82ebcaf2f6141028c1946ae6cbf418572a42
Simon Zhou <simon.zhou@ucla.edu>
2018-02-07 02:19:54 -0800

    Finishedd selenium functionality

--- selenium/buttons.py
+++ selenium/buttons.py
@@ -11,9 +11,13 @@ driver.get('localhost:3000')
 infoButton = driver.find_element_by_class_name('info-button-container')
 all_children_by_css = infoButton.find_elements_by_css_selector("*")
 all_children_by_css[0].click()
+menu = driver.find_element_by_class_name('info-menu')
+if menu.is_displayed():
+    print 'info menu is visible'
 
+# we must test functionality of home button in Jest
 
 ################# Quit Selenium on Success #################
 
-# print "successful"
-# driver.quit()
+print "successful"
+driver.quit()
--- selenium/main.py
+++ selenium/main.py
@@ -1,7 +1,7 @@
 import sys
-sys.dont_write_bytecode = True
+sys.dont_write_bytecode = True # put on every file to ensure that no temp file is created
 
 import prelim
 import selection
 import toggle
-# import buttons
+import buttons
