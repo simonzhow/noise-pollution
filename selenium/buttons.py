4d5b6cc354914a1f588949e48144193a45debdc4
Simon Zhou <simon.zhou@ucla.edu>
2018-02-06 22:56:17 -0800

    Finished most dynamic testing

---
+++ selenium/buttons.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome('/Users/simonzhou/Documents/chromedriver')
+driver.get('localhost:3000')
+
+################# Info Button #################
+infoButton = driver.find_element_by_class_name('info-button-container')
+all_children_by_css = infoButton.find_elements_by_css_selector("*")
+all_children_by_css[0].click()
+
+
+################# Quit Selenium on Success #################
+
+# print "successful"
+# driver.quit()
